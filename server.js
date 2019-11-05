c5f082422763d688a074831ac8b935016f6ecc4c
paul bennett <luapkb@gmail.com>
2019-11-05 11:30:58 -0800

    location obj

--- server.js
+++ server.js
@@ -10,16 +10,25 @@ const app = express();
 const PORT = process.env.PORT || 5500;
 
 app.get('/location', (req, res) =>{
-  const geoData = req('.data/geo.json');
-res.send(geoData.results.geometry)
+  const geoData = require('./data/geo.json');
+  console.log (geoData);
+  const city = req.query.data;
+  console.log(city);
+  const locationData = new GeoLocation(city, geoData);
+  console.log(locationData);
+  res.send(locationData);
+
 });
 
 
-// function GeoLocation(city, geoData){
-//   this.GeoLocation;
-//   this.;
-//   this.;
-//   }
+ function GeoLocation(city, geoData){
+  this.search_query = city; 
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+
 
 app.listen(PORT,() => console.log(`im alive on PORT, ${PORT}`));
 
